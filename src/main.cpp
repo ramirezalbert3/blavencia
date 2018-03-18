5f8d161134b859f634a291864620deace4deb129
Albert <ramirezalbert3@gmail.com>
2018-03-18 11:42:58 +0100

    trying to create matrix class and integrate with googletest

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,17 @@
 #include <SFML/Graphics.hpp>
-
+#include <components/map.hpp>
 
 int main()
 {
     sf::RenderWindow world(sf::VideoMode(500, 500), "Blavencia");
     world.setFramerateLimit(60);
+    
+//     using ct = cell_t::type;
+//     
+//     map_t<3, 3> map {   {ct::wall, ct::wall,  ct::wall},
+//                         {ct::wall, ct::empty, ct::wall},
+//                         {ct::wall, ct::door,  ct::wall}
+//                         };
 
     while (world.isOpen())
     {
